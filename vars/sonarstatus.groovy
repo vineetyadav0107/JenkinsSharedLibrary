e09bb13243ea44b605371bcc76ef0bea0d59ec1e
vineetyadav0107 <73833155+vineetyadav0107@users.noreply.github.com>
2023-03-21 19:46:37 +0530

    Update sonarstatus.groovy

--- vars/sonarstatus.groovy
+++ vars/sonarstatus.groovy
@@ -1,5 +1,6 @@
 def call(projectKey){
     def qg_status = null
+    echo ${projectKey}
     withCredentials([usernameColonPassword(credentialsId: 'sonar-access', variable: 'USERPASS')]) {
       sh '''
            echo "${projectKey}"
