ca69e028360a732691ad62e40a1639c02dce1b45
vineetyadav0107 <73833155+vineetyadav0107@users.noreply.github.com>
2023-03-21 19:08:41 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,16 +1,16 @@
 def call(projectKey,projectName,loginKey,coveragePath,sonarHost){
     def scannerHome = tool 'SonarScanner';
-                  withSonarQubeEnv("SonarQube-Localhost") {
-                  sh "${tool("SonarScanner")}/bin/sonar-scanner \
-                  -Dsonar.projectKey=${projectKey} \
-                  -Dsonar.projectName=${projectName} \
-                  -Dsonar.projectVersion=1.0 \
-                  -Dsonar.sources=. \
-                  -Dsonar.sonar.nodejs.executable=. \
-                  -Dsonar.language=. \
-                  -Dsonar.tests=src \
-                  -Dsonar.test.inclusions=. \
-                  -Dsonar.host.url=${sonarHost} \
-				  -Dsonar.javascript.lcov.reportPaths=${coveragePath} \
-                  -Dsonar.login=${loginKey}"
+         withSonarQubeEnv("SonarQube-Localhost") {
+         sh "${tool("SonarScanner")}/bin/sonar-scanner \
+         -Dsonar.projectKey=${projectKey} \
+         -Dsonar.projectName=${projectName} \
+         -Dsonar.projectVersion=1.0 \
+         -Dsonar.sources=. \
+         -Dsonar.sonar.nodejs.executable=. \
+         -Dsonar.language=. \
+         -Dsonar.tests=src \
+         -Dsonar.test.inclusions=. \
+         -Dsonar.host.url=${sonarHost} \
+	 -Dsonar.javascript.lcov.reportPaths=${coveragePath} \
+         -Dsonar.login=${loginKey}"
 }
