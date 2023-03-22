dcafcb20fad9c1570a48ec66c9b1d517d7c40d6d
vineetyadav0107 <73833155+vineetyadav0107@users.noreply.github.com>
2023-03-22 11:26:04 +0530

    Update sonarstatus.groovy

--- vars/sonarstatus.groovy
+++ vars/sonarstatus.groovy
@@ -3,11 +3,7 @@ def call(projectKey){
     echo "${projectKey}"
     withCredentials([usernameColonPassword(credentialsId: 'sonar-access', variable: 'USERPASS')]) {
       sh """
-           echo "${projectKey}"
-           echo "${USERPASS}"
-           set x
            sleep 15s
-           status="OK"
        """
        def res = sh(script: "curl --user ${USERPASS} https://ci-builds.compunnel.com/sonar/api/qualitygates/project_status?projectKey=${projectKey} | jq -r '.projectStatus.status'", returnStdout: true)
        return res
